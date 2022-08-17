8fed7398a069893b9f4a0bb5654430878b2d71e7
bhumi.desai <bhumi.desai@1rivet.com>
2022-08-17 18:28:25 +0530

    feat: life cycle hooks

--- src/employees/components/employees-list-container/employees-list-presentation/employees-list.presentation.vue
+++ src/employees/components/employees-list-container/employees-list-presentation/employees-list.presentation.vue
@@ -1,10 +1,42 @@
 <template>
+<div>
+  <p>{{msg}}</p>
   <p>Employees List</p>
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 export default defineComponent({
-    name:"EmployeeListPresentation"
+    name:"EmployeeListPresentation",
+    data() {
+      return {
+        msg: "Hello World",
+      }
+    },
+    beforeCreate() {
+      console.log('beforeCreate hook called');
+    },
+    created() {
+      console.log('created hook called');
+    },
+    beforeMount() {
+      console.log('beforeMount hook called');
+    },
+    mounted() {
+      console.log('mounted has been called'); 
+    },
+    beforeUpdate(){
+       console.log('beforeUpdate hook called');
+    },
+    updated(){
+       console.log('updated hook called');
+    },
+    // beforeDestroy() {
+    //    console.log('beforeDestroy hook called');
+    // },
+    // destroyed() {
+    //    console.log('destroyed hook called')
+    // }
 })
 </script>
